921d8a26cdf1bf0ad5c9aeac2b023affd3386f7e
SalladShooter <139503084+SalladShooter@users.noreply.github.com>
2023-12-29 17:45:01 +0000

    Fixed Database to Save to Different Users

--- main.py
+++ main.py
@@ -1,63 +1,80 @@
 from flask import Flask, render_template, request, jsonify
 import json
+import os
+import logging
 
 app = Flask(__name__)
 
-# Path to the JSON file to store tasks
-JSON_FILE_PATH = 'database/data.json'
+logging.basicConfig(level=logging.INFO)
 
-def load_tasks():
+def get_replit_username():
+    username = os.environ['REPL_OWNER']
+    logging.info(f"Retrieved username: {username}")
+    return username
+
+def get_json_file_path(username):
+    return f'database/{username}_data.json'
+
+def load_tasks(username):
+    json_file_path = get_json_file_path(username)
     try:
-        with open(JSON_FILE_PATH, 'r') as file:
+        with open(json_file_path, 'r') as file:
             tasks = json.load(file)
     except FileNotFoundError:
-        tasks = {'tasks': []}  # Initialize tasks as a dictionary with a 'tasks' key
-    return tasks.get('tasks', [])  # Return the 'tasks' key or an empty list if it doesn't exist
+        tasks = {'tasks': []}
+    return tasks.get('tasks', [])
 
-def save_tasks(tasks):
-    with open(JSON_FILE_PATH, 'w') as file:
-        json.dump({'tasks': tasks}, file)  # Save tasks as a dictionary with a 'tasks' key
+def save_tasks(username, tasks):
+    json_file_path = get_json_file_path(username)
+    with open(json_file_path, 'w') as file:
+        json.dump({'tasks': tasks}, file) 
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-    tasks = load_tasks()
+    username = get_replit_username()
+    tasks = load_tasks(username)
     return jsonify(tasks)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    logging.info(f"Request method: {request.method}")
+    username = get_replit_username()
+
     if request.method == 'POST':
         task_data = request.form.get('task_data')
         if task_data:
-            tasks = load_tasks()
+            tasks = load_tasks(username)
             tasks.append({'taskText': task_data, 'completed': False})
-            save_tasks(tasks)
+            save_tasks(username, tasks)
             return jsonify({'status': 'success', 'message': 'Task added'})
 
-    tasks = load_tasks()
+    tasks = load_tasks(username)
     return render_template('index.html', tasks=tasks)
 
 @app.route('/delete_task', methods=['POST'])
 def delete_task():
+    username = get_replit_username()
     task_data = request.form.get('task_data')
     if task_data:
-        tasks = load_tasks()
+        tasks = load_tasks(username)
         if task_data in [task['taskText'] for task in tasks]:
             tasks = [task for task in tasks if task['taskText'] != task_data]
-            save_tasks(tasks)
+            save_tasks(username, tasks)
             return jsonify({'status': 'success', 'message': 'Task deleted'})
 
     return jsonify({'status': 'error', 'message': 'Task not found'})
 
 @app.route('/update_task', methods=['POST'])
 def update_task():
+    username = get_replit_username()
     task_data = request.form.get('task_data')
     completed = request.form.get('completed') == 'true'
     if task_data:
-        tasks = load_tasks()
+        tasks = load_tasks(username)
         for task in tasks:
             if task['taskText'] == task_data:
                 task['completed'] = completed
-                save_tasks(tasks)
+                save_tasks(username, tasks)
                 return jsonify({'status': 'success', 'message': 'Task updated'})
 
     return jsonify({'status': 'error', 'message': 'Task not found'})
--- static/script.js
+++ static/script.js
@@ -2,7 +2,27 @@ document.addEventListener('DOMContentLoaded', function () {
     var createTaskButton = document.querySelector('.create-task');
     var taskList = document.querySelector('.task-list');
 
-    // Function to add a task to the list
+    createTaskButton.addEventListener('click', function () {
+        var taskText = prompt('Enter task name:');
+        if (taskText) {
+            var formData = new URLSearchParams();
+            formData.append('task_data', taskText);
+
+            fetch('/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: formData,
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Server response:', data);
+                addTask(taskText, false);
+            });
+        }
+    });
+
     function addTask(taskText, completed) {
         var taskElement = document.createElement('div');
         taskElement.className = 'task';
@@ -14,40 +34,31 @@ document.addEventListener('DOMContentLoaded', function () {
         cancelIconElement.className = 'material-symbols-outlined cancel-icon';
         cancelIconElement.textContent = 'cancel';
 
-        // Set the 'completed' class based on the completed parameter
         if (completed) {
             taskElement.classList.add('completed');
         }
 
-        // Click event listener for each task element
         taskElement.addEventListener('click', function () {
-            // Toggle the 'completed' class on the clicked task
             taskElement.classList.toggle('completed');
+            updateTaskStatus(taskText, taskElement.classList.contains('completed'));
+        });
 
-            // Make an AJAX request to the Flask server to update the task status
+        function updateTaskStatus(taskText, completed) {
             fetch('/update_task', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
                 body: 'task_data=' + encodeURIComponent(taskText) +
-                    '&completed=' + taskElement.classList.contains('completed'),
+                    '&completed=' + completed,
             })
                 .then(response => response.json())
                 .then(data => {
-                    // Handle the response if needed
                     console.log('Server response:', data);
                 });
-        });
+        }
 
-        // Click event listener for the cancel icon
-        cancelIconElement.addEventListener('click', function (event) {
-            event.stopPropagation();  // Prevent the taskElement click event from firing
-
-            // Remove the task from the DOM
-            taskList.removeChild(taskElement);
-
-            // Make an AJAX request to the Flask server to delete the task
+        function deleteTask(taskText) {
             fetch('/delete_task', {
                 method: 'POST',
                 headers: {
@@ -57,41 +68,21 @@ document.addEventListener('DOMContentLoaded', function () {
             })
                 .then(response => response.json())
                 .then(data => {
-                    // Handle the response if needed
                     console.log('Server response:', data);
                 });
+        }
+
+        cancelIconElement.addEventListener('click', function (event) {
+            event.stopPropagation();
+            deleteTask(taskText);
+            taskList.removeChild(taskElement);
         });
 
         taskElement.appendChild(taskTextElement);
         taskElement.appendChild(cancelIconElement);
-
         taskList.appendChild(taskElement);
     }
 
-    // Click event listener for the "create task" button
-    createTaskButton.addEventListener('click', function () {
-        var taskText = prompt('Enter task name:');
-        if (taskText) {
-            // Make an AJAX request to the Flask server to save the new task
-            fetch('/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: 'task_data=' + encodeURIComponent(taskText),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    // Handle the response if needed
-                    console.log('Server response:', data);
-
-                    // Add the task to the front-end task list with the completed status
-                    addTask(taskText, false);
-                });
-        }
-    });
-
-    // Load existing tasks from the server
     fetch('/tasks')
         .then(response => response.json())
         .then(tasks => {
